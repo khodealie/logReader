1417fe2d6dbfaddf9af4fe8ed73933aa72e6024f
Ali Sohrabi <khodealie@gmail.com>
2023-01-28 13:51:07 +0330

    - add log routes and config RouteServiceProvider.php - count function in LogController.php added - add CountRes.php resource - add ServiceExists.php rule

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\CountRes;
+use App\Models\Log;
+use App\Rules\ServiceExists;
 use Illuminate\Http\Request;
 
 class LogController extends Controller
 {
-    //
+    public function count(Request $request)
+    {
+        $request->validate([
+            'serviceNames' => [new ServiceExists],
+            'statusCode' => ['bail','numeric', 'max:599', 'min:100'],
+            'startDate' => ['date'],
+            'endDate' => ['bail','date', 'after:startDate']
+        ]);
+        return new CountRes(log::filterViaParams($request->query()));
+    }
 }
--- app/Models/Log.php
+++ app/Models/Log.php
@@ -15,4 +15,39 @@ class Log extends Model
         'service' => LogServices::class,
         'method' => LogMethods::class
     ];
+
+    public static function filterViaParams(array $listParams)
+    {
+        $newInstance = new static();
+        foreach ($listParams as $query => $params) {
+
+            if ($query == 'serviceNames' || $query == 'serviceName')
+                $newInstance = (is_array($params)) ?
+                    $newInstance->whereIn('service', $params) :
+                    $newInstance->where('service', $params);
+
+            if ($query == 'statusCodes' || $query == 'statusCode')
+                $newInstance = (is_array($params)) ?
+                    $newInstance->whereIn('status', $params) :
+                    $newInstance->where('status', $params);
+
+            if ($query == 'methods' || $query == 'method')
+                $newInstance = (is_array($params)) ?
+                    $newInstance->whereIn('method', $params) :
+                    $newInstance->where('method', $params);
+
+            if ($query == 'endpoints' || $query == 'endpoint')
+                $newInstance = (is_array($params)) ?
+                    $newInstance->whereIn('endpoint', $params) :
+                    $newInstance->where('endpoint', $params);
+
+            if ($query == 'startDate')
+                $newInstance = $newInstance->whereDate('created_at', '>=', $params);
+
+            if ($query == 'endDate')
+                $newInstance = $newInstance->whereDate('created_at', '<=', $params);
+        }
+        return $newInstance;
+    }
+
 }
